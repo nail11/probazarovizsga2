67e44451efffd7abdcda10bdb40bb8d0ca9e6934
nail11 <nail@t-online.hu>
2021-08-04 16:45:13 +0200

    müködik a guess_the_number.py

--- testproject/guess_the_number.py
+++ testproject/guess_the_number.py
@@ -15,8 +15,8 @@ from datetime import datetime
 # import pprint
 
 options = Options()
-#options.add_argument('--headless')
-#options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 
 # In order for ChromeDriverManager to work you must pip install it in your own environment.
 
@@ -39,31 +39,55 @@ try:
     guess_click = 0
     min = 0
     max = 100
-    #neg_num = -19
-    #high_num = 255
-    err_message1 = "A számláló értéke eltér az app számláló értékétől !"
-    message1 = "A számlálók értékei megegyeznek:\n"
+    neg_num = -19
+    high_num = 255
+    err_message1 = "A szám < 0, az app összeomlott!"
+    message1 =  "A szám < 0, az app nem omlott össze!"
+    err_message2 = "A szám > 100, az app összeomlott!"
+    message2 =  "A szám > 100, az app nem omlott össze!"
+    err_message3 = "A számláló értéke eltér az app számláló értékétől !"
+    message3 = "A számlálók értékei megegyeznek:\n"
 
     while (equal_field.get_attribute('class') == "alert alert-success ng-hide"):
         input_field.clear()
         number = int(((max - min) / 2) + min)
         d = int(max - number)
-        input_field.send_keys(number)
-        guess_button.click()
-        guess_click = guess_click + 1
-        if int(max - number) > 1:
-            if lower_field.get_attribute('class') == "alert alert-warning":
-                max = number
-            if higher_field.get_attribute('class') == "alert alert-warning":
-                min = number
-        else:
-            assert equal_field.get_attribute('class') == "alert alert-success"
-            break
+        for num in (neg_num, high_num, number):
+            if num == neg_num:
+                input_field.clear()
+                input_field.send_keys(neg_num)
+                guess_button.click()
+                guess_click = guess_click + 1
+                assert higher_field.get_attribute('class') == "alert alert-warning",err_message1
+            elif num == high_num:
+                input_field.clear()
+                input_field.send_keys(high_num)
+                guess_button.click()
+                guess_click = guess_click + 1
+                assert lower_field.get_attribute('class') == "alert alert-warning", err_message2
+            else:
+                input_field.clear()
+                #number = int(((max - min) / 2) + min)
+                #d = int(max - number)
+                input_field.send_keys(number)
+                guess_button.click()
+                guess_click = guess_click + 1
+                if int(max - number) > 1:
+                    if lower_field.get_attribute('class') == "alert alert-warning":
+                        max = number
+                    if higher_field.get_attribute('class') == "alert alert-warning":
+                        min = number
+                else:
+                    assert equal_field.get_attribute('class') == "alert alert-success"
+                break
     num_gess = driver.find_element_by_xpath("/html/body/div/div[3]/p/span").text
 
-    assert str(guess_click) == num_gess, err_message1
-    print(message1)
+    assert str(guess_click) == num_gess, err_message3
+
+    print(message1+"\n")
+    print(message2+"\n")
+    print(message3)
     print(f"{guess_click} = {num_gess}")
 
 finally:
-    pass
+    driver.close()
