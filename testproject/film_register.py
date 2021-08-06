2bf39ac95dbfce9af72d0e5ff07bef1d0ede5805
nail11 <nail@t-online.hu>
2021-08-06 21:26:47 +0200

    javított

--- testproject/film_register.py
+++ testproject/film_register.py
@@ -1,5 +1,6 @@
 # environment settings
 # ......................................................................
+import time
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,6 +12,7 @@ from selenium.webdriver.common.by import By
 from datetime import datetime
 
 from selenium.webdriver.common.keys import Keys
+
 # from datetime import date
 # import time
 # from pathlib import Path
@@ -18,8 +20,8 @@ from selenium.webdriver.common.keys import Keys
 
 
 options = Options()
-#options.add_argument('--headless')
-#options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 
 # In order for ChromeDriverManager to work you must pip install it in your own environment.
 
@@ -28,51 +30,55 @@ driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 url = "https://witty-hill-0acfceb03.azurestaticapps.net/film_register.html"
 
-
-# elements
-#  reg_butt = driver.find_element_by_xpath("/html/body/div[2]/div[1]/button")
-
 # variables
 
 number_of_films = 24
-a_number = number_of_films+1
-film_title = "Black widow"
-release_year = 2021
-chronological_year_of_events = 2020
-trailer_url = "https://www.youtube.com/watch?v=Fp9pNPdNwjI"
-image_url = "https://m.media-amazon.com/images/I/914MHuDfMSL._AC_UY327_FMwebp_QL65_.jpg"
-film_summary = "https://www.imdb.com/title/tt3480822/"
+a_list_num = number_of_films + 1
+b_list_num = a_list_num + 1
 err_message1 = "A katalógusban lévő filmek száma nem 24"
 message1 = "A katalógusban lévő filmek száma 24"
+err_message2 = "A katalógusban lévő filmek új száma nem 25"
+message2 = "A katalógusban lévő filmek új száma 25"
+entry_elements = ["nomeFilme", "anoLancamentoFilme", "anoCronologiaFilme", "linkTrailerFilme",
+                  "linkImagemFilme", "linkImdbFilme"]
+entry_items = ["Black widow", 2021, 2020, "https://www.youtube.com/watch?v=Fp9pNPdNwjI",
+               "https://m.media-amazon.com/images/I/914MHuDfMSL._AC_UY327_FMwebp_QL65_.jpg",
+               "https://www.imdb.com/title/tt3480822/"]
 
 try:
 
     driver.get(url)
 
-    film_list = WebDriverWait(driver, 5).until(lambda driver: len(driver.find_elements_by_tag_name('a')) == a_number)
+
+    # functions
+
+    def entry_filler(list1, list2):
+        driver.find_element_by_xpath("/html/body/div[2]/div[1]/button").click()
+        time.sleep(2)
+        for i, entry in enumerate(list1):
+            e = entry
+            driver.find_element_by_xpath(f"//*[@id='{entry}']").send_keys(list2[i])
+            time.sleep(1)
+        driver.find_element_by_xpath("/html/body/div[2]/div[2]/fieldset/button[1]").click()
+
+
+    # TC 01 = length of film list in the register = 24
+
+    film_list = WebDriverWait(driver, 5).until(lambda driver: len(driver.find_elements_by_tag_name('a')) == a_list_num)
     assert film_list, err_message1
+    print("TC 01")
     print(message1)
+    print()
 
-    driver.find_element_by_xpath("/html/body/div[2]/div[1]/button").click()
+    # TC 02   after adding a new element, the new length of list register = 25
 
-    #JavaScript = "driver.find_element_by_xpath('/html/body/div[2]/div[2]/fieldset/button[1]').click();"
-    #driver.execute_script(JavaScript)
+    entry_filler(entry_elements, entry_items)
 
-    cim1 = driver.find_element_by_id("nomeFilme")
-    cim2 = driver.find_element_by_xpath('//*[@id="nomeFilme"]')
-    driver.find_element_by_id("nomeFilme").send_keys("film_title")
-    driver.find_element_by_xpath('//*[@id="nomeFilme"]').send_keys("film_title")
-    print(cim1)
-    print(cim2)
-    driver.find_element_by_xpath('//*[@id=""nomeFilme"]').send_keys("film_title")
-    driver.find_element_by_xpath('//*[@id="anoLancamentoFilme"]').send_keys("release_year")
-    driver.find_element_by_xpath('//*[@id="anoCronologiaFilme"]').send_keys("chronological_year_of_events")
-    driver.find_element_by_xpath('//*[@id="linkTrailerFilme"]').send_keys("trailer_url")
-    driver.find_element_by_xpath('//*[@id="linkImagemFilme"]').send_keys("image_url")
-    driver.find_element_by_xpath('//*[@id="linkImdbFilme"]').send_keys("film_summary")
+    # film_list = WebDriverWait(driver, 5).until(lambda driver: len(driver.find_elements_by_tag_name('a')) == b_list_num)
 
-    #driver.find_element_by_xpath('/html/body/div[2]/div[2]/fieldset/button[1]').click()
-
+    assert len(driver.find_elements_by_tag_name('a')) == b_list_num, err_message2
+    print("TC 02")
+    print(message2)
 
 finally:
-    pass
+    driver.close()
